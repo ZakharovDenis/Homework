c2c8f1352aa33fec68532d1d16e29a770828c6ab
ZakharovDenis <z.d.a.52rus@mail.ru>
2024-07-18 22:28:38 +0300

    Add files via upload

---
+++ home work 2.py
@@ -0,0 +1,8 @@
+homework = 12
+hours_spent = 1.5
+course_name =('Python')
+time_for_one_homework = hours_spent/ homework
+print(course_name,'всего задач',
+homework,'затрачено часов',hours_spent,
+'среднее время выполнения',time_for_one_homework,
+'часа')
